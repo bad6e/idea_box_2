14a0dde8e19ff2ad6fd8aee4ceddea723097932c
Bret Doucette <doucette.bret@gmail.com>
2015-12-01 13:51:52 -0700

    Refactored getideas

--- app/assets/javascripts/ideas/getideas.js
+++ app/assets/javascripts/ideas/getideas.js
@@ -1,19 +1,18 @@
 function getIdeas(){
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost:3000/api/v1/ideas',
-    dataType: 'json',
-    success: function(response){
-      $.each(response, function(i, idea) {
-        renderIdea(idea)
-      })
-    }
-  })
-}
+  $.getJSON('http://localhost:3000/api/v1/ideas', function(response){
+    parseIdeas(response)
+  });
+};
+
+function parseIdeas(response) {
+  $.each(response, function(i, idea) {
+    renderIdea(idea)
+  });
+};
 
 function renderIdea(idea) {
   $('#latest-posts').prepend(
      "<h3>" + idea.title + "</h3>"
      + "<p>" + idea.body + "</p>"
-  )
-}
+  );
+};
