f5c95f3669df5df3da7111ec8d73c14568ab70f9
Bret Doucette <doucette.bret@gmail.com>
2015-12-01 09:05:23 -0700

    Added in creating new idea

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost:3000/api/v1/ideas',
-    dataType: 'json',
-    success: function(response){
-      $.each(response, function(i, idea) {
-        renderIdea(idea)
-      })
-    }
-  })
+  getIdeas()
+  createIdea()
 });
 
 
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,6 +2,16 @@ class Api::V1::IdeasController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Idea.all
+    respond_with Idea.all, location: nil
+  end
+
+  def create
+    respond_with Idea.create(idea_params), location: nil
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
   end
 end
