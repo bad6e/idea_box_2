456cf848bc8778cfd1db408f51c632d1f40330fa
Bret Doucette <doucette.bret@gmail.com>
2015-12-02 14:49:14 -0700

    Updated put test

--- spec/controllers/api/v1/ideas_controller_spec.rb
+++ spec/controllers/api/v1/ideas_controller_spec.rb
@@ -33,8 +33,8 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     it "deletes an idea on the idea list" do
       delete :destroy, id: @idea_one.id ,format: :json
       assert_response :success
-      get :index ,format: :json
-      expect(response_data.count).to eq(3)
+
+      expect(Idea.count).to eq(3)
     end
   end
 
@@ -43,8 +43,19 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     it "creates an idea on the idea list" do
       post :create, idea: { title: "E", body: "E1" }, format: :json
       assert_response :success
-      get :index ,format: :json
-      expect(response_data.count).to eq(5)
+
+      expect(Idea.count).to eq(5)
+    end
+  end
+
+  describe "PUT /api/v1/ideas/:id" do
+
+    it "creates an idea on the idea list" do
+      put :update, format: :json, id: @idea_one.id, idea: { title: "Hi", body: "Mom" }
+      assert_response :success
+
+      idea_title = Idea.find(@idea_one.id).title
+      expect(idea_title).to eq("Hi")
     end
   end
 end
