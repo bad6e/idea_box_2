2a12a853bcde2326797b939e7e4052fa4e7501fe
Bret Doucette <doucette.bret@gmail.com>
2015-12-02 14:01:40 -0700

    Setup spec rails  helper

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:idea_one) {
+    Idea.create(title: "Zipskee", body: "Travel App")
+  }
+
+  let(:idea_two) {
+    Idea.create(title: "Build Stuff", body: "Buy it at Ikea")
+  }
+
+  it "is valid" do
+    expect(idea_one).to be_valid
+  end
+
+  it "is invalid without a title" do
+    idea_one.title = nil
+    expect(idea_one).to be_invalid
+  end
+
+  it "is invalid without a body" do
+    idea_one.body = nil
+    expect(idea_one).to be_invalid
+  end
+
+  it "is invalid without a quality" do
+    idea_one.quality = nil
+    expect(idea_one).to be_invalid
+  end
 end
